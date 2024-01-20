50aed242236850fef4de4a5392c784971849323d
Carlos Cabi <carlangascamachobrigido@gmail.com>
2024-01-20 15:58:26 -0500

    Adding changes to `cors.js` middleware

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,13 +1,14 @@
 import cors from "cors"
 
 export const ACCEPTED_ORIGINS = [
+  process.env.ACCEPTED_CLIENT_ORIGIN,
   "http://localhost:3000",
   "http://localhost:5500",
+  "http://localhost:5173",
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
-  origin: (origin, callback) =>{
-  
+  origin: (origin, callback) => {
     if (acceptedOrigins.includes(origin)) {
       return callback(null, true)
     }
